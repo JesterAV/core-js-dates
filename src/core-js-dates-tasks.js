36d93a84a1e88df77cc8557a443e03ff9655d9e3
Aleksei Bulash <winchesrers@gmail.com>
2026-01-06 02:35:29 +0300

    feat: implement getNextFridayThe13th function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -238,8 +238,22 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const result = new Date(date.getTime());
+
+  result.setDate(13);
+
+  if (result.getTime() <= date.getTime()) {
+    result.setMonth(result.getMonth() + 1);
+    result.setDate(13);
+  }
+
+  while (result.getDay() !== 5) {
+    result.setMonth(result.getMonth() + 1);
+    result.setDate(13);
+  }
+
+  return result;
 }
 
 /**
